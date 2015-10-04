6a36ab9f370700c3aad6b519814058b346855f9a
Léo Lam <leo@innovatetechnologi.es>
2015-10-04 11:35:49 +0200

    messageEventDirective: Use $apply() before broadcasting
    Angular doesn't automatically call $apply() on $broadcast(), so we have
    to call it ourselves.

--- src/app/message-event-directive.js
+++ src/app/message-event-directive.js
@@ -1,14 +1,16 @@
-angular.module("player").directive("messageEvent", ($window) => {
+angular.module("player").directive("messageEvent", ($window, $scope) => {
   return {
     link(scope) {
       $window.addEventListener("message", ({ origin = "", data = {}} = {}) => {
-        if (!origin.includes("https://control.shoutca.st") && !origin.includes("http://localhost")) {
-          return;
-        }
-        if (data.type === "reloadConfig") {
-          return scope.$broadcast("message::" + data.type, data.config);
-        }
-        return scope.$broadcast("message::unknown", data);
+        $scope.$apply(() => {
+          if (!origin.includes("https://control.shoutca.st") && !origin.includes("http://localhost")) {
+            return;
+          }
+          if (data.type === "reloadConfig") {
+            return scope.$broadcast("message::" + data.type, data.config);
+          }
+          return scope.$broadcast("message::unknown", data);
+        });
       });
     },
   };
